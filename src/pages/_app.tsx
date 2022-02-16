a896bd4317bc0f5c3768dae2cb2ba4c1e71fe0e8
AdrianKnapp <adrknapp@gmail.com>
2022-02-15 22:36:43 -0300

    Added prettier and eslint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,8 @@
+import { AppProps } from 'next/app';
 import './styles/globals.scss';
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Component {...pageProps} />
-  )
+function MyApp({ Component, pageProps }: AppProps) {
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
